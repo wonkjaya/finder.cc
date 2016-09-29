d033337c5904cfc9d9434690504752678958d4e4
vm <rohmanmail@gmail.com>
2016-09-29 20:29:46 +0700

    s

--- application/modules/admin/views/lumino/objekLokasi.php
+++ application/modules/admin/views/lumino/objekLokasi.php
@@ -26,7 +26,7 @@
 		</div><!--/.row-->
 		
 		<div class="row">
-			<div class="col-md-10">
+			<div class="col-md-12">
 			 <div class="panel panel-primary">
 				<div class="panel-body">
 					<table class="table table-bordered">
